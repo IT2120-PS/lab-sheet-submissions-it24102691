e6e680868a50e577b1e8e591b5256fa9720922e9
it24102691 <it24102691@my.sliit.lk>
2025-09-02 09:58:56 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24102691Lab6/it24102691Lab6.R
@@ -0,0 +1,23 @@
+setwd("C:\\Users\\it24102691\\Desktop\\IT24102691Lab6")
+#Question 01
+#part 01
+#Random variable x has binomial distribution with n=50 and p=0.85
+
+#part 02
+#probability of getting an exact value can be calculate using this command
+dbinom(47,50,0.85)
+
+
+#Question 02
+#part 01
+#x=number of calls receive by the call center per hour 
+
+#part 02
+#poisson Distribution
+#Random variable x has poisson distribution with landa=12
+
+#part 3
+#p(x=15)
+dpois(15,12)
+
+
